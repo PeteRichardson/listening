4895d72245effe20df0d18f986a4fd9f14b1b5f8
Pete Richardson <pete@peterichardson.com>
2024-06-11 13:13:42 -0700

    rust - added lsof command to get real deduped data.
    Not sorted by port yet
    -c Does not show full command... just lsof lines

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
+use std::collections::HashSet;
 use std::error::Error;
+use std::fmt;
+use std::hash::{Hash, Hasher};
+use std::process::Command;
 use tabled::{
     settings::{object::Columns, themes::Colorization, Alignment, Color, Style},
     Table, Tabled,
@@ -22,42 +26,92 @@ struct Listener {
     fd: String,
     user: String,
     node: String,
-    inaddr: String,
     #[tabled(rename = "INADDR")]
+    inaddr: String,
     action: String,
     #[tabled(skip)]
     full_command: String,
 }
 
 impl Listener {
-    fn new(lsof_line: String) -> Self {
+    fn new(lsof_line: &str) -> Self {
+        let splits: Vec<&str> = lsof_line.split_ascii_whitespace().collect();
+        //println!("{:?}", splits);
+
+        let (inaddr, port) = splits[8].split_once(':').expect("couldn't split");
+
         Self {
-            command: lsof_line.clone(),
-            port: 5000,
-            pid: 716,
-            fd: String::from("10u"),
-            user: String::from("pete"),
-            node: String::from("TCP"),
-            inaddr: String::from("127.0.0.1"),
-            action: String::from("LISTEN"),
-            full_command: lsof_line.clone(),
+            command: splits[0].to_string().replace("\\x20", " "),
+            port: port.parse::<u32>().unwrap(),
+            pid: splits[1].parse::<u32>().unwrap(),
+            fd: splits[3].to_string(),
+            user: splits[2].to_string(),
+            node: splits[7].to_string(),
+            inaddr: inaddr.to_string(),
+            action: "LISTEN".to_string(), // todo: trim parens off this value
+            full_command: lsof_line.to_string(),
         }
+    }
+}
+
+impl fmt::Display for Listener {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}:{}:{}:{}:{}:{}:{}:{}",
+            self.command,
+            self.port,
+            self.pid,
+            self.fd,
+            self.user,
+            self.node,
+            self.inaddr,
+            self.action
+        )
+    }
+}
+impl PartialEq for Listener {
+    fn eq(&self, other: &Self) -> bool {
+        self.port == other.port
+    }
+}
+impl Eq for Listener {}
+impl Hash for Listener {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.command.hash(state);
+        self.pid.hash(state);
+        self.port.hash(state);
+        self.inaddr.hash(state);
     }
 }
 
 #[derive(Clone)]
 struct ListenerList {
-    listeners: Vec<Listener>,
+    listeners: HashSet<Listener>,
 }
 
 impl ListenerList {
     fn new() -> Self {
-        Self {
-            listeners: vec![
-                Listener::new("Stream Deck".to_string()),
-                Listener::new("node".to_string()),
-            ],
+        let mut listeners: HashSet<Listener> = HashSet::new();
+
+        //'lsof -nP +c 0 -i4' command returns lines like the following:
+        //node                        10166 pete   31u  IPv4 0xe4ad34249b227fc5      0t0  TCP 127.0.0.1:45623 (LISTEN)
+
+        let lsof_lines = Command::new("lsof")
+            .arg("-nP")
+            .arg("+c")
+            .arg("0")
+            .arg("-i4")
+            .output()
+            .unwrap();
+        let stdout = String::from_utf8(lsof_lines.stdout).expect("bad stdout from lsof command");
+        for line in stdout.lines().skip(1).collect::<HashSet<_>>() {
+            if line.ends_with("(LISTEN)") {
+                listeners.insert(Listener::new(line));
+            }
         }
+        //ListenerList::sort_by_port_order(&mut listeners);
+        Self { listeners }
     }
 }
 
